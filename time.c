000123bb8ffd3c1863a53540bbc100d4547b0ed5
JustaDoc <103592528+JustaDoc@users.noreply.github.com>
2024-10-18 16:52:03 -0400

    time implemented

--- time.c
+++ time.c
@@ -2,6 +2,12 @@
 
 double elapsed_time(struct timeval* start_time, struct timeval* end_time){
     //TODO: return the difference between end_time and start_time.
-    
-    return 0.0;
+    double start = start_time -> tv_usec + (start_time->tv_sec * 1000000.0);
+
+    double fin = end_time -> tv_usec + (end_time->tv_sec * 1000000.0);
+
+    double val = (fin-start)/1000000.0;
+
+    //return 0.0;
+    return val;
 }
